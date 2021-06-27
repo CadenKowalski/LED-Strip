a1c12939c3c78f3d9f5620259c669d818341f021
Caden Kowalski <cadenk@Cadens-MacBook-Pro.local>
2021-06-27 16:38:53 -0400

    Created project (0.0.0)

---
+++ LED Strip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LED Strip
+//
+//  Created by Caden Kowalski on 6/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
